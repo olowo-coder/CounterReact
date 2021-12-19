67295e40860f9e61a228415a3af37b7090765e23
olowo-coder <olowofela.olanrewaju@decagon.dev>
2021-12-19 14:46:46 +0100

    third

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,23 +1,48 @@
 import React, { Component } from 'react';
+import { Fragment } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 0
+        count: 2,
+        tags: ['tag1', 'tag2', 'tag3']
+        // imageUrl: 'https://picsum.photos/200'
+    };
+
+    styles = {
+        fontSize: 10,
+        borderRaduis: 200,
+        backgroundColor: '#d74',
+        fontWeight: 'bolder'
     };
 
     render() { 
+
         return (
-            <React.Fragment>
-                <span>{this.formatCounter()}</span>
-                
-                <button>Increment</button>
-            </React.Fragment>
+            <div>
+                {/* <img src={this.state.imageUrl} alt="" /> */}
+                {/* <span style={this.styles} className="badge badge-primary m-2">{this.formatCounter()}</span> */}
+                <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
+                <button className="btn btn-secondary btn-sm">Increment</button> 
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul> 
+            </div>
+
+            // <React.Fragment>
+                // Write your code here when using React,Fragment
+            // </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count) === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCounter(){
         const {count} = this.state;
-        return count === 0 ? <h1>ZERO</h1> : count;
+        return count === 0 ? <h3>Zero</h3> : count;
     }
 }
  
