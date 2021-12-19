68c3a6feba90ead4acc1652a4bd0ff835ceef3a7
olowo-coder <olowofela.olanrewaju@decagon.dev>
2021-12-19 15:35:37 +0100

    third

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@ import { Fragment } from 'react';
 
 class Counter extends React.Component {
     state = {
-        count: 2,
+        count: 1,
         tags: ['tag1', 'tag2', 'tag3']
         // imageUrl: 'https://picsum.photos/200'
     };
@@ -15,17 +15,41 @@ class Counter extends React.Component {
         fontWeight: 'bolder'
     };
 
+    // constructor(){
+    //     super();
+    //     console.log("Constructor", this);
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    renderTags(){
+        if (this.state.tags.length === 0 ) return <p>There are no tags !</p>;
+
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>; 
+    }
+
+    handleIncrement = (product) => {
+        console.log(product);
+        console.log("button Cliked");
+        // this.state.count ++;
+        this.setState({count: this.state.count + 1});
+    };
+
+    doHandlerIncrement = () => {
+        this.handleIncrement({id: 1})
+    };
+
     render() { 
 
         return (
             <div>
                 {/* <img src={this.state.imageUrl} alt="" /> */}
                 {/* <span style={this.styles} className="badge badge-primary m-2">{this.formatCounter()}</span> */}
+                 {/* {this.state.tags.length === 0 && 'Please create a new tag! '} */}
+
                 <span className={this.getBadgeClasses()}>{this.formatCounter()}</span>
-                <button className="btn btn-secondary btn-sm">Increment</button> 
-                <ul>
-                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-                </ul> 
+                <button onClick={() => this.handleIncrement({id: 1})} className="btn btn-secondary btn-sm">Increment</button> 
+                {this.renderTags()}
+                
             </div>
 
             // <React.Fragment>
